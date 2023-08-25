1918ece049ece3d5a301435102725ff8c9d02f25
Abdullah Essam <121309175+Abdallah-Essam-Abdallah@users.noreply.github.com>
2023-08-25 19:47:20 +0300

    Update payment_constants.dart

--- lib/features/payment/constants/payment_constants.dart
+++ lib/features/payment/constants/payment_constants.dart
@@ -3,7 +3,7 @@ class PaymentConstants {
   static const String getAuthToken = '/auth/tokens';
   static const String getOrderId = '/ecommerce/orders';
   static const String getPaymentRequest = '/acceptance/payment_keys';
-  static const String integrationCardId = '3974136';
+  static const String integrationCardId = 'inregrationCardId';
   static const String apiKey =
-      'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T0RNNU9UWTBMQ0p1WVcxbElqb2lhVzVwZEdsaGJDSjkuZTlVV0pGdE5iWE1SOExHU1BZSWV5bDgtQlNKbHpLbUFsNnJTSjFpNG8tZTNRQ3RvSzJHOWJSdFF4TTBTdzh5ek01dEo5cTZxTEczQ2hOU1dRT0xnV1E=';
+      'apiKey';
 }
