39e7d94d951ec79a7160ce0a43c67da45c11c197
Abdullah Essam <121309175+Abdallah-Essam-Abdallah@users.noreply.github.com>
2023-08-26 19:56:57 +0300

    Update user.dart

--- lib/features/authentication/domain/entity/user.dart
+++ lib/features/authentication/domain/entity/user.dart
@@ -2,14 +2,19 @@ import 'package:equatable/equatable.dart';
 
 // ignore: must_be_immutable
 class UserEntity extends Equatable {
-  final String? userName;
-  final String? userId;
-  final String? email;
-  final String? phoneNumber;
+  final String userName;
+  final String userId;
+  final String email;
+  final String phoneNumber;
   String? image;
 
-  UserEntity(
-      this.userName, this.userId, this.email, this.phoneNumber, this.image);
+  UserEntity({
+    required this.userName,
+    required this.userId,
+    required this.email,
+    required this.phoneNumber,
+    this.image,
+  });
 
   @override
   List<Object?> get props => [
