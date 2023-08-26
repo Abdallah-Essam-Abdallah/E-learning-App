f862f25235cc98efe9206eb9a3981fec1fbecaf0
Abdullah Essam <121309175+Abdallah-Essam-Abdallah@users.noreply.github.com>
2023-08-26 19:57:24 +0300

    Update user_model.dart

--- lib/features/authentication/data/model/user_model.dart
+++ lib/features/authentication/data/model/user_model.dart
@@ -2,15 +2,20 @@ import 'package:courses_app/features/authentication/domain/entity/user.dart';
 
 // ignore: must_be_immutable
 class UserModel extends UserEntity {
-  UserModel(super.userName, super.userId, super.email, super.phoneNumber,
-      super.image);
+  UserModel({
+    required super.userName,
+    required super.userId,
+    required super.email,
+    required super.phoneNumber,
+    super.image,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
-        json['displayName'],
-        json['userId'],
-        json['email'],
-        json['phoneNumber'],
-        json['image'],
+        userName: json['displayName'],
+        userId: json['userId'],
+        email: json['email'],
+        phoneNumber: json['phoneNumber'],
+        image: json['image'],
       );
 
   Map<String, dynamic> toJson() => {
